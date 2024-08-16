6d89deff261d7618a417c41ac78423fa28dd46c6
IJU666 <fauzimaulanabandung2020@gmail.com>
2024-08-16 19:00:11 +0700

    update

--- resources/views/navbar/navbar.blade.php
+++ resources/views/navbar/navbar.blade.php
@@ -1,16 +1,19 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-color: ;">
   <div class="container-fluid col-lg-11">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <img src="{{ asset("img/jiwacode.png") }}" alt="" srcset="" width="50px" class="navbar-brand">
     <div class="text-end">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse text-start" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#">Features</a>
-        <a class="nav-link" href="#">Pricing</a>
-        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        <a class="nav-link active" aria-current="page" href="#">Tentang Kami</a>
+        <a class="nav-link" href="#">Portofolio</a>
+        <a class="nav-link" href="#">Kontak</a>
+        <a class="nav-link">Disabled</a>
+        <a class="form-check form-switch nav-lin col-start-2 m-auto border">
+            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked>
+        </a>
       </div>
       </div>
     </div>
